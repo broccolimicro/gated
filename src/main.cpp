fdeed7f55a65348a568c0f35212083e36354c861
Ned Bingham <eab242@cornell.edu>
2020-12-17 17:18:42 -0500

    options, input and output seem to be functional

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,40 @@
+#include <fstream>
+#include <iostream>
 #include <common/standard.h>
 #include <boolean/number.h>
 #include <interpret_boolean/export.h>
+#include <interpret_boolean/import.h>
 #include <synthesize/boolean.h>
+#include <parse_expression/expression.h>
 
 using namespace boolean;
 
-void print_xfactor(unsigned_int n, ucs::variable_set vars)
+void print_xfactor(string name, int index, unsigned_int n, ucs::variable_set vars)
 {
 	for (int i = 0; i < n.bits.size(); i++)
 	{
 		parse_expression::expression result = export_expression_xfactor(n.bits[i], vars);
-		cout << "p" << i << ":\t" << result.to_string() << endl;
+		cout << name << index << "[" << i << "]:\t" << result.to_string() << endl;
 	}
 	cout << endl;
 }
 
-
-void print_hfactor(unsigned_int n, ucs::variable_set vars)
+void print_hfactor(string name, int index, unsigned_int n, ucs::variable_set vars)
 {
 	for (int i = 0; i < n.bits.size(); i++)
 	{
 		parse_expression::expression result = export_expression_hfactor(n.bits[i], vars);
-		cout << "p" << i << ":\t" << result.to_string() << endl;
+		cout << name << index << "[" << i << "]:\t" << result.to_string() << endl;
 	}
 	cout << endl;
 }
 
-void print(unsigned_int n, ucs::variable_set vars)
+void print(string name, int index, unsigned_int n, ucs::variable_set vars)
 {
 	for (int i = 0; i < n.bits.size(); i++)
 	{
 		parse_expression::expression result = export_expression(n.bits[i], vars);
-		cout << "p" << i << ":\t" << result.to_string() << endl;
+		cout << name << index << "[" << i << "]:\t" << result.to_string() << endl;
 	}
 	cout << endl;
 }
@@ -39,64 +42,160 @@ void print(unsigned_int n, ucs::variable_set vars)
 void print_help()
 {
 	printf("Usage: gated [options] file...\n");
-	printf("A logic minimization engine.\n");
+	printf("A self-contained logic synthesis engine.\n");
 	printf("\nGeneral Options:\n");
 	printf(" -h,--help      Display this information\n");
 	printf("    --version   Display version information\n");
-	printf(" -v,--verbose   Display verbose messages\n");
-	printf(" -d,--debug     Display internal debugging messages\n");
 	printf("\nConversion Options:\n");
-	printf(" -g <file>      Convert this HSE to an hse-graph and save it to a file\n");
-	printf(" -eg <file>     Convert this HSE to an elaborated hse-graph and save it to a file\n");
-	printf(" -pn <file>     Convert this HSE to a petri-net and save it to a file\n");
-	printf(" -sg <file>     Convert this HSE to a state-graph and save it to a file\n");
+	printf(" -i <inputs>      Comma separated list of variable definitions <name>:<type>. Type is 'u' for unsigned int, 'i' for signed int, or 'f' for float followed by the bitwidth. For example: \"a:u8,b:i4,x:u5\"\n");
+	printf(" -o <outputs>     Comma separated list of expressions. For example: \"a+b,b<<x\"\n");
 }
 
 void print_version()
 {
-	printf("hsesim 1.0.0\n");
-	printf("Copyright (C) 2013 Sol Union.\n");
+	printf("gated 1.0.0\n");
+	printf("Copyright (C) 2020 Ned Bingham.\n");
 	printf("There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
 	printf("\n");
 }
 
 
-int main()
+int main(int argc, char **argv)
 {
 	ucs::variable_set vars;
-	for (int i = 0; i < 4; i++)
+	map<string, boolean::unsigned_int> u;
+	map<string, boolean::signed_int> i;
+	string input, output;
+
+	tokenizer parser(false);
+	parse_expression::expression::register_syntax(parser);
+
+	for (int i = 1; i < argc; i++)
 	{
-		int index = vars.define(ucs::variable());
-		vars.nodes[index].name.push_back(ucs::instance("a", {i}));
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		int index = vars.define(ucs::variable());
-		vars.nodes[index].name.push_back(ucs::instance("b", {i}));
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		int index = vars.define(ucs::variable());
-		vars.nodes[index].name.push_back(ucs::instance("y", {i}));
+		string arg = argv[i];
+		if (arg == "--help" || arg == "-h")			// Help
+		{
+			print_help();
+			return 0;
+		}
+		else if (arg == "--version")	// Version Information
+		{
+			print_version();
+			return 0;
+		}
+		else if (arg == "-i")
+		{
+			i++;
+			if (i < argc) {
+				if (input.size() > 0)
+					input += ",";
+				input += argv[i];
+			} else {
+				error("", "expected input specification", __FILE__, __LINE__);
+				return 1;
+			}
+		}
+		else if (arg == "-o")
+		{
+			i++;
+			if (i < argc) {
+				if (output.size() > 0)
+					output += ",";
+				output += argv[i];
+			} else {
+				error("", "expected output specification", __FILE__, __LINE__);
+				return 1;
+			}
+		}
+		else if (arg == "-f")
+		{
+			i++;
+			if (i < argc) {
+				ifstream f;
+				f.open(argv[i], std::ios::in);
+				string line;
+				while (getline(f, line)) {
+					if (line.size() > 3 and strncmp(line.c_str(), ".i ", 3) == 0) {
+						if (input.size() > 0) {
+							input += ",";
+						}
+						input += line.substr(3);
+					} else if (line.size() > 3 and strncmp(line.c_str(), ".o ", 3) == 0) {
+						if (output.size() > 0) {
+							output += ",";
+						}
+						output += line.substr(3);
+					}
+				}
+
+				f.close();
+			} else {
+				error("", "expected specification file", __FILE__, __LINE__);
+				return 1;
+			}
+		}
 	}
 
-	boolean::unsigned_int a(4, 0);
-	boolean::unsigned_int b(4, 4);
-	boolean::unsigned_int y(5, 8);
-	boolean::unsigned_int p =
-		((b == 1) & (a + y))/* |
+	int start = 0, end = 0;
+	do {
+		end = input.find(":", start);
+		string name = input.substr(start, end - start);
+		start = end+1;
+		end = input.find(",", start);
+		string type = input.substr(start, end - start);
+		int width = atoi(type.c_str()+1);
+		int offset = vars.nodes.size();
+		for (int j = 0; j < width; j++)
+		{
+			int index = vars.define(ucs::variable());
+			vars.nodes[index].name.push_back(ucs::instance(name, {j}));
+		}
+		if (type[0] == 'u') {
+			u.insert(pair<string, boolean::unsigned_int>(name, boolean::unsigned_int(width, offset)));
+		} else if (type[0] == 'i') {
+			i.insert(pair<string, boolean::signed_int>(name, boolean::signed_int(width, offset)));
+		} else {
+			error("", string("unsupported data type '") + type[0] + "'", __FILE__, __LINE__);
+		}
+		start = end+1;
+	} while (start > 0);
+
+	list<boolean::unsigned_int> result;
+
+	start = 0;
+	end = 0;
+	while (end != string::npos) {
+		end = output.find(",", start);
+		string exp_str = output.substr(start, end - start);
+		start = end+1;
+		parser.insert("", exp_str);
+		parse_expression::expression exp(parser);
+		if (parser.is_clean()) {
+			result.push_back(import_unsigned_int(exp, u, 0, &parser));
+		}
+		parser.reset();
+	}
+
+	/*boolean::unsigned_int p =
+		((b == 1) & (a + y)) |
 		((b == 2) & ((a<<1) + y)) |
 		((b == 3) & ((a<<2) + (~a) + y)) |
 		((b == 4) & ((a<<2) + y)) |
 		((b == 5) & ((a<<2) + a + y)) |
 		((b == 6) & ((a<<3) + (~a<<1) + y)) |
 		((b == 7) & ((a<<3) + (~a) + y)) | 
-		((b == 8) & ((a<<3) + y))*/;
-	p.simplify();
-	print_hfactor(p, vars);
-	printf("\n\n");
+		((b == 8) & ((a<<3) + y));
+	p.simplify();*/
 	
-	vector<int> hide;// = {0, 1, 2, 3, 8, 9, 10, 11, 12};
+	int index = 0;
+	for (list<boolean::unsigned_int>::iterator i = result.begin(); i != result.end(); i++) {
+		i->simplify();
+		print_xfactor("r", index, *i, vars);
+		printf("\n\n");
+		index++;
+	}
+	
+	/*vector<int> hide;// = {0, 1, 2, 3, 8, 9, 10, 11, 12};
 	map<cube, int> factors;
 	int sz = 0;
 	
@@ -111,6 +210,6 @@ int main()
 		cout << vars.nodes[i->second].to_string() << ":\t" << result.to_string() << endl << endl;
 	}
 
-	print_xfactor(p, vars);
+	print_xfactor(p, vars);*/
 }
 
